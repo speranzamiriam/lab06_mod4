3d185847b1c406a27485c9345581f524827e5fd0
Esperanza Miriam Aquino <speranzamiriam1@gmail.com>
2023-03-30 18:58:54 -0400

    laboratorio06
    funcional

---
+++ config.app.ts
@@ -0,0 +1,7 @@
+export const TESTDATA = {
+    email: 'speranzamiriam2@gmail.com',
+    password: '#qwerty#$$##',
+    url: 'https://todoist.com/auth/login',
+    apiUrl: 'https://api.todoist.com/rest/v2/',
+    apiToken: 'd574c2359cb59a80ac4fbfa4a2fcdf38a58cf06e',
+}
